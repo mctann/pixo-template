1e7d6b34ba568bf6cb98b4ccdf29f0f3356f5f2d
mctan <tanmichelle5@gmail.com>
2023-03-25 17:03:11 +0700

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#6196FF",
+        secondary: "#FB7A8E",
+      },
+      fontFamily: {
+        inter: ["inter"],
+      },
+    },
+  },
+  plugins: [],
+};
